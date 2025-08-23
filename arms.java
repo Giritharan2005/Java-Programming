3834e01a4b3a9cf27397aee372e8ccc2d272a873
Giritharan2005 <166808731+Giritharan2005@users.noreply.github.com>
2025-08-23 09:52:46 +0530

    Add files via upload

---
+++ arms.java
@@ -0,0 +1,29 @@
+public class armstron
+{
+  static int digit_count(int n){
+  int count=0;
+  while (n!= 0) {
+  count++;
+   n/=10;
+}
+   return count;
+}
+public static void main(String[] args) {
+   int n = 370;
+   int temp=n;
+   int sum=0;
+   int rem;
+   int digit=digit_count(n);
+   while(n!= 0) {
+   rem=n%10;
+   sum+=Math.pow(rem, digit);
+   n/=10;
+   }
+  if(temp==sum) {
+     System.out.println("Armstrong number");
+}
+  else{
+     System.out.println("Not armstrong number");
+  }
+}
+}
