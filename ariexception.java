972891ea8273ad17cf113aefc072e9097c2a89fb
Giritharan2005 <166808731+Giritharan2005@users.noreply.github.com>
2025-08-23 09:51:57 +0530

    Add files via upload

---
+++ ariexception.java
@@ -0,0 +1,13 @@
+public class ariexception {
+public static void main(String[] args) {
+int a = 10;
+int b = 0;
+if(b==0){
+throw new ArithmeticException ("cannot be divided by zero");
+}
+else{
+int c=a/b;
+System.out.println(c);
+}
+}
+}
