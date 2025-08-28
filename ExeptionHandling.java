1b6f452e02c9fa36341e5197db8a83b8bf7c615f
Giritharan2005 <166808731+Giritharan2005@users.noreply.github.com>
2025-08-28 20:40:39 +0530

    Add files via upload

---
+++ ExeptionHandling.java
@@ -0,0 +1,14 @@
+public class ExeptionHandling {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        try{
+            int c=a/b;
+            System.out.println(c);
+        }
+catch(Exception e) {
+System.out.println(e.getMessage());
+}
+System.out.println("hello");
+}
+}
