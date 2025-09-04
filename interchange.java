4c75747c096f5cc6b0bf0700b2d73b47a7df72cc
Giritharan2005 <166808731+Giritharan2005@users.noreply.github.com>
2025-09-04 19:24:14 +0530

    Add files via upload

---
+++ interchange.java
@@ -0,0 +1,18 @@
+public class interchange {
+     public static void main(String[] args){
+        int a=5;
+        int b=6;
+        System.out.println("The value of a is "+a);
+        System.out.println("The value of b is "+b);
+        System.out.println("After Swapping");
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("The value of a is "+a);
+        System.out.println("The value of b is "+b);
+     }
+
+
+
+    
+}
