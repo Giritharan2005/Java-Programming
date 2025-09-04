76eb8148b23b704a087f2184ce62d2e37da10fca
Giritharan2005 <166808731+Giritharan2005@users.noreply.github.com>
2025-09-04 19:24:42 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,15 @@
+public class palindrome {
+    public static void main(String[] args) {
+        String s="mad";
+        String rev="";
+        for(int i=s.length()-1; i >= 0 ;i--){
+            rev+=s.charAt(i);
+        }
+        if(s.equals(rev)){
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("not a palindrome");
+        }
+    }
+}
