f12e607c5e54b7ceffb8eb4df18a9aaa1c5447e0
Giritharan2005 <166808731+Giritharan2005@users.noreply.github.com>
2025-09-26 09:10:27 +0530

    Add files via upload

---
+++ treesetex.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class treesetex {
+    public static void main(String[] args) {
+        TreeSet<Object> l=new TreeSet<>();
+        l.add(5);
+        l.add(3);
+        l.add(4);
+        l.add(1);
+        l.add(2);
+        l.add("hello");
+        System.out.println(1);
+    }
+}
