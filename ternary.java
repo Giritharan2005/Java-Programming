b4e461d685f730978d8c255c3804726c140c268e
Giritharan2005 <166808731+Giritharan2005@users.noreply.github.com>
2025-09-26 09:10:07 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,8 @@
+public class ternary {
+    public static void main(String[] args){
+        int time = 17;
+        String result = (time < 20) ? "Good ." : "Bad .";
+        System.out.println(result);
+    }
+
+}
