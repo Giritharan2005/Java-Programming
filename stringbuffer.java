8e2c024f9e194a9daa9f3798711bfdffa82ba285
Giritharan2005 <166808731+Giritharan2005@users.noreply.github.com>
2025-09-26 09:08:39 +0530

    Add files via upload

---
+++ stringbuffer.java
@@ -0,0 +1,8 @@
+public class stringbuffer  {
+public static void main(String[] args) {
+StringBuffer sb=new StringBuffer("hello");
+StringBuilder sbd=new StringBuilder("welcome");
+System.out.println(sb.append("programming"));
+System.out.println(sb.reverse());
+}
+}
